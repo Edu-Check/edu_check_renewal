0526d06014375fd59e93766d1929489f5a9ec787
seungyeon <seun9yeon@naver.com>
2025-03-20 20:25:54 +0900

    [EC-26] feat: 로그인 기능 구현 (#30)

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,6 +1,33 @@
 import React from 'react'
 import styles from "./Login.module.css"
+import InputBox from '../../components/inputBox/InputBox';
+import MainButton from '../../components/buttons/mainButton/MainButton';
 
 export default function Login() {
-  return <div className={container`${styles.login}`}>Login</div>;
+  const handleChange = () => {
+
+  }
+  
+  return (
+  <div className={styles.login}>
+    <div className={styles.logoImage}>
+      <img src="./assets/logo.png" alt="user image" />
+    </div>
+    <InputBox
+    type="email"
+    title="이메일"
+    disabled={false}
+    onChange={handleChange}
+    ></InputBox>
+    <InputBox
+    type="password"
+    title="비밀번호"
+    disabled={false}
+    onChange={handleChange}
+    ></InputBox>
+    <div className={styles.loginButton}>
+    <MainButton title="로그인"></MainButton>
+    </div>
+  </div>
+  );
 }
