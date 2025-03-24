85ed75c91834451140198fe0e4e193e8e6faf3b4
DaeJungYoon <dbseowjd12@gmail.com>
2025-03-25 04:11:29 +0900

    [EC-74] FE/feat: 학습자 출석 현황 페이지 UI 구현 (#70)
    * [EC-74] feat: 학습자 출석 현황 조회 페이지 레이아웃 구성
    
    - DashBoard 컴포넌트 item 제거
    
    * [EC-74] feat: 학습자 출석 현황 wip
    
    * [EC-74] feat: 학습자 출석 현황 페이지 UI 구현

--- client/src/pages/dashBoard/DashBoard.jsx
+++ client/src/pages/dashBoard/DashBoard.jsx
@@ -5,7 +5,6 @@ import styles from './DashBoard.module.css';
 
 import Tab from '../../components/tab/Tab';
 import SideBar from '../../components/sideBar/SideBar';
-import DashBoardItem from '../../components/dashBoardItem/DashBoardItem';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
@@ -22,7 +21,6 @@ export default function DashBoard() {
       <SideBar />
       <div className={styles.dashBoardBox}>
         <Tab />
-
         {/* TODO : dashBoardContent 내부에 대시보드 및 컴포넌트 사용 */}
         <div className={styles.dashBoardContent}>
           <Outlet></Outlet>
--- client/src/pages/studentAttendance/StudentAttendance.jsx
+++ client/src/pages/studentAttendance/StudentAttendance.jsx
@@ -1,8 +1,68 @@
 import React from 'react'
 import styles from "./StudentAttendance.module.css"
-
+import DashBoardItem from '../../components/dashBoardItem/DashBoardItem';
+import DataBoard from '../../components/dataBoard/DataBoard';
+import ProgressBar from '../../components/progressBar/ProgressBar';
+import Calendar from '../../components/calendar/calendar';
 export default function StudentAttendance() {
+  const attendanceRatio = 56;
+  const lateCount = 3;
+  const earlyLeaveCount = 0;
+  const absenceCount = 0;
+  const cumulativeAbsenceCount = 1;
+  const value = 84;
+  const startDate = '2024-10-07';
+  const endDate = '2025-04-04';
+  const attendanceData = [
+    { date: '2025-03-04', status: 'ABSENCE' },
+    { date: '2025-03-05', status: 'EARlY_LEAVE' },
+    { date: '2025-03-06', status: 'LATE' },
+    { date: '2025-03-07', status: 'ABSENCE' },
+    { date: '2025-03-10', status: 'ABSENCE' },
+    { date: '2025-03-13', status: 'LATE' },
+    { date: '2025-03-17', status: 'ABSENCE' },
+    { date: '2025-03-20', status: 'LATE' },
+    { date: '2025-03-21', status: 'LATE' },
+    { date: '2025-03-19', status: 'EARlY_LEAVE' },
+    { date: '2025-03-25', status: 'EARlY_LEAVE' },
+    { date: '2025-03-28', status: 'EARlY_LEAVE' },
+    { date: '2025-03-14', status: 'EARlY_LEAVE' },
+  ];
   return (
-    <div>StudentAttendance</div>
-  )
+    <>
+      <div className={styles.dashBoardItemDiv}>
+        <div className={styles.attendanceStatistics}>
+          <div className={styles.attendanceRatio}>
+            <DashBoardItem width={'100%'}>
+              <p className={styles.headingText}>출석률 {attendanceRatio}%</p>
+              <div className={styles.progressBarBottom}>
+                <ProgressBar
+                  value={value}
+                  max={100}
+                  startDate={startDate}
+                  endDate={endDate}
+                ></ProgressBar>
+              </div>
+            </DashBoardItem>
+          </div>
+          <div className={styles.attendanceCurrent}>
+            <DashBoardItem width={'100%'}>
+              <p className={styles.headingText}>결석 현황</p>
+              <div className={styles.attendanceType}>
+                <DataBoard title="지각" data={`${lateCount}회`}></DataBoard>
+                <DataBoard title="조퇴" data={`${earlyLeaveCount}회`}></DataBoard>
+                <DataBoard title="결석" data={`${absenceCount}회`}></DataBoard>
+                <DataBoard title="누적 결석" data={`${cumulativeAbsenceCount}회`}></DataBoard>
+              </div>
+            </DashBoardItem>
+          </div>
+        </div>
+        <div className={styles.attendanceCalendar}>
+          <DashBoardItem width={'100%'}>
+            <Calendar attendanceData={attendanceData}></Calendar>
+          </DashBoardItem>
+        </div>
+      </div>
+    </>
+  );
 }
