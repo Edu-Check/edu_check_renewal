8a67e55fbcb44d1e3b9ca407df471bb53285e24d
seungyeon <seun9yeon@naver.com>
2025-03-25 18:53:44 +0900

    [EC-98] feat: 과정 관리자 회의실 예약 UI 구현 (#79)

--- client/src/components/dashBoardItem/DashBoardItem.jsx
+++ client/src/components/dashBoardItem/DashBoardItem.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import styles from "./DashBoardItem.module.css"
 
-export default function DashBoardItem({ width, children }) {
-  return <div style={{ width: `${width}` }} className={styles.DashBoardItem}>{children}</div>;
+export default function DashBoardItem({ width, title }) {
+  return (
+    <div style={{ width: `${width}` }} className={styles.DashBoardItem}>{title}</div>
+  );
 }
--- client/src/pages/staffRoomReservation/StaffRoomReservation.jsx
+++ client/src/pages/staffRoomReservation/StaffRoomReservation.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import styles from "./StaffRoomReservation.module.css"
+import DashBoardItem from "../../components/dashBoardItem/DashBoardItem"
 
 export default function StaffRoomReservation() {
   return (
-    <div>StaffRoomReservation</div>
+    <div className={styles.roomDashBoardItem}>
+      <DashBoardItem title="예약 현황"></DashBoardItem>
+    </div>
   )
 }
