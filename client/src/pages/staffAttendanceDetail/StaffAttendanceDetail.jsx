10c0d483bbdd184e3c118a1ad9e85028d018260f
DaeJungYoon <dbseowjd12@gmail.com>
2025-03-26 12:25:30 +0900

    [EC-94] FE/feat: 관리자 세부 출결 현황 UI 작업 (#93)
    * [EC-94] feat: 관리자 세부 출석 현황 wip
    
    * [EC-94] feat: 관리자 세부 출결 현황 페이지 UI 구현
    
    * [EC-94] chore: 불필요한 코드 제거

--- client/src/pages/staffAttendanceDetail/StaffAttendanceDetail.jsx
+++ client/src/pages/staffAttendanceDetail/StaffAttendanceDetail.jsx
@@ -1,8 +1,62 @@
-import React from 'react'
-import styles from "./StaffAttendanceDetail.module.css"
+import React, { useState } from 'react';
+import styles from './StaffAttendanceDetail.module.css';
+import DashBoardItem from '../../components/dashBoardItem/DashBoardItem';
+import BaseListItem from '../../components/listItem/baseListItem/BaseListItem';
+import DataBoard from '../../components/dataBoard/DataBoard';
 
 export default function StaffAttendanceDetail() {
+  const dummyUser = {
+    name: '홍길동',
+    phone: '010-1234-5678',
+  };
+
+  const attendanceItems = [
+    { id: 1, content: '2024년 01월 01일', tagTitle: '결석' },
+    { id: 2, content: '2024년 01월 01일', tagTitle: '조퇴' },
+    { id: 3, content: '2024년 01월 01일', tagTitle: '출석' },
+    { id: 4, content: '2024년 01월 01일', tagTitle: '출석' },
+    { id: 5, content: '2024년 01월 01일', tagTitle: '지각' },
+    { id: 6, content: '2024년 01월 01일', tagTitle: '결석' },
+    { id: 7, content: '2024년 01월 01일', tagTitle: '결석' },
+    { id: 8, content: '2024년 01월 01일', tagTitle: '결석' },
+    { id: 9, content: '2024년 01월 01일', tagTitle: '조퇴' },
+    { id: 9, content: '2024년 01월 01일', tagTitle: '조퇴' },
+    { id: 9, content: '2024년 01월 01일', tagTitle: '조퇴' },
+    { id: 9, content: '2024년 01월 01일', tagTitle: '조퇴' },
+    { id: 9, content: '2024년 01월 01일', tagTitle: '출석' },
+  ];
+
   return (
-    <div>StaffAttendanceDetail</div>
-  )
+    <div className={styles.container}>
+      <div className={styles.dashboardContainer}>
+        <DashBoardItem>
+          <div className={styles.userInfoContainer}>
+            <div className={styles.userInfo}>
+              <h2 className={styles.userName}>{dummyUser.name}</h2>
+              <p className={styles.userPhone}>{dummyUser.phone}</p>
+            </div>
+          </div>
+
+          <div className={styles.databoardContainer}>
+            <DataBoard title="금일 기준 출석률" data="0%" />
+            <DataBoard title="전체 출석률" data="0%" />
+            <DataBoard title="과정 진행률" data="0%" />
+          </div>
+        </DashBoardItem>
+      </div>
+
+      <div className={styles.contentWrapper}>
+        <div className={styles.listContainer}>
+          {attendanceItems.map((item) => (
+            <BaseListItem
+              key={item.id}
+              id={item.id}
+              content={item.content}
+              tagTitle={item.tagTitle}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
