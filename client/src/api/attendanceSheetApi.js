d29129c82f05604a6e2c3b600ef74c99dce66fae
ysc13245 <147698362+ysc13245@users.noreply.github.com>
2025-03-31 18:18:06 +0900

    [EC-183] FE/feat 출석부 다운로드 api 연동 (#202)
    * [EC-183] feat: Attendance 컬럼 수정
    
    * [EC-183] feat: flask 연동 테스트
    
    * [EC-183] feat: flask 연동 테스트
    
    * [EC-183] fix: 오타 수정
    
    * [EC-183] fix: axios 수정 테스트
    
    * [EC-183] feat: 출석부 api 분리
    
    * [EC-183] fix: 누락된 imnport 수정
    
    * [EC-183] fix: 임포트 경로 수정

--- client/src/api/attendanceSheetApi.js
+++ client/src/api/attendanceSheetApi.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-import store from '../../store/store';
+import store from '../store/store';
+
 
 const apiInstance = axios.create({
   baseURL: import.meta.env.VITE_APP_URL,
