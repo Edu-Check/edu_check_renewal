d9412836cda1083b45f3dcc2cfa0e3ab04f8ce42
ysc13245 <ysc13245@gmail.com>
2025-04-12 00:44:00 +0900

    [ECR-11] fix: 로그인 쿼리문 수정

--- api/src/main/java/org/example/educheck/domain/attendance/service/AttendanceService.java
+++ api/src/main/java/org/example/educheck/domain/attendance/service/AttendanceService.java
@@ -92,13 +92,14 @@ public class AttendanceService {
 
         Attendance attendance = attendanceRepository.findByStudentIdTodayCheckInDate(student.getId())
                 .orElseThrow(() -> new ResourceNotFoundException("금일 출석 기록이 없습니다."));
-        attendance.checkOut(currentTime);
-
         Campus campus = attendance.getLecture().getCourse().getCampus();
+
         if (!campus.isWithinDistance(requestDto.getLongitude(), requestDto.getLatitude())) {
 
             throw new InvalidRequestException("출석/퇴실 가능한 위치가 아닙니다.");
         }
+
+        attendance.checkOut(currentTime);
 
         return new AttendanceStatusResponseDto(attendance.getAttendanceStatus());
     }
--- api/src/main/java/org/example/educheck/domain/member/repository/MemberRepository.java
+++ api/src/main/java/org/example/educheck/domain/member/repository/MemberRepository.java
@@ -16,20 +16,25 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     boolean existsByEmail(String email);
 
     @Query("""
-                select new org.example.educheck.domain.member.dto.LoginResponseDto(
-                     m.id, m.email, m.name, m.phoneNumber, m.birthDate, c.id, cr.id, cr.name,  m.lastLoginDateTime, m.lastPasswordChangeDateTime, CASE WHEN a.id IS NOT NULL THEN true ELSE false END)
-                from Member m
+                SELECT DISTINCT new org.example.educheck.domain.member.dto.LoginResponseDto(
+                     m.id, m.email, m.name, m.phoneNumber, m.birthDate, c.id, cr.id, cr.name,
+                     m.lastLoginDateTime, m.lastPasswordChangeDateTime,
+                     CASE WHEN a.id IS NOT NULL THEN true ELSE false END)
+                FROM Member m
                 LEFT JOIN Student s ON s.member.id = m.id
                 LEFT JOIN Registration r ON r.student.id = s.id
                 LEFT JOIN Course cr ON r.course.id = cr.id AND NOW() BETWEEN cr.startDate AND cr.endDate
                 LEFT JOIN Campus c ON cr.campus.id = c.id
-                LEFT JOIN Attendance a ON a.student.id = s.id AND a.lecture.date = CURRENT_DATE
+                LEFT JOIN Attendance a ON a.student.id = s.id
+                LEFT JOIN Lecture l ON l.id = a.lecture.id
+                    AND l.date = CURRENT_DATE
                 WHERE m.id = :memberId
             """)
     Optional<LoginResponseDto> studentLoginResponseDtoByMemberId(@Param("memberId") Long memberId);
 
+
     @Query("""
-            select new org.example.educheck.domain.member.dto.LoginResponseDto(
+            select DISTINCT new org.example.educheck.domain.member.dto.LoginResponseDto(
                  m.id, m.email, m.name, m.phoneNumber, m.birthDate, c.id, cr.id, cr.name,  m.lastLoginDateTime, m.lastPasswordChangeDateTime, null
             )
             from Member m
