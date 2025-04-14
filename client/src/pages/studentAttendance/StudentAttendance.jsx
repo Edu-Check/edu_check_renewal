a1b6d094d1c26ae7bf6f63aa5acf88e92a5ac95b
ysc13245 <147698362+ysc13245@users.noreply.github.com>
2025-04-15 04:54:49 +0900

    [ECR-31] FE/feat: 수강생 출석 현황 달력 데이터 API 연동 (#33)
    * [ECR-31] feat: 강의가 존재하지 않는 기간 에러메세지 처리 및 API 호출시 월, 별
    관리
    
    * [ECR-31] feat: 출석 데이터 표시
    
    * [ECR-31] feat: 출석 현황 달력 출석 추가
    
    * [ECR-31] feat: 라우터 최신화
    
    * [ECR-31] fix: 출석부 css 수정
    
    ---------
    
    Co-authored-by: yejin <yejinoh58@gmail.com>

--- client/src/pages/studentAttendance/StudentAttendance.jsx
+++ client/src/pages/studentAttendance/StudentAttendance.jsx
@@ -79,17 +79,8 @@ export default function StudentAttendance() {
           selectedMonth,
         );
 
-        if (response && response.data) {
-          setCourseInfo({
-            name: response.data.courseName,
-          });
-
-          const formattedData = response.data.attendanceList.map((item) => ({
-            date: item.lectureDate,
-            status: item.attendanceStatus,
-          }));
-
-          setAttendanceData(formattedData);
+        if (response) {
+          setAttendanceCalendarData(response.records);
         }
       } catch (error) {
         const errorMessage = error.response?.data?.message ?? '오류가 발생했습니다.';
