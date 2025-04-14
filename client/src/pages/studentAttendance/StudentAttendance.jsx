2e6b775ca44f95651e6e70748c5d0e6b0ebd15f8
ysc13245 <147698362+ysc13245@users.noreply.github.com>
2025-04-15 03:13:19 +0900

    [ECR-31] FE/feat: 수강생-출석-현황-달력-데이터-API-연동 (#30)
    * [ECR-31] feat: 강의가 존재하지 않는 기간 에러메세지 처리 및 API 호출시 월, 별
    관리
    
    * [ECR-31] feat: 출석 데이터 표시
    
    * [ECR-31] feat: 출석 현황 달력 출석 추가
    
    ---------
    
    Co-authored-by: yejin <yejinoh58@gmail.com>

--- client/src/pages/studentAttendance/StudentAttendance.jsx
+++ client/src/pages/studentAttendance/StudentAttendance.jsx
@@ -21,17 +21,21 @@ export default function StudentAttendance() {
     endDate: ' ',
   });
 
-  const [courseInfo, setCourseInfo] = useState({
-    name: '',
-  });
-
-  const [attendanceData, setAttendanceData] = useState([]);
+  const [attendanceCalendarData, setAttendanceCalendarData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
 
   const currentDate = new Date();
   const currentYear = currentDate.getFullYear();
   const currentMonth = currentDate.getMonth() + 1;
+
+  const [selectedYear, setSelectedYear] = useState(currentYear);
+  const [selectedMonth, setSelectedMonth] = useState(currentMonth);
+
+  const handleMonthChange = (year, month) => {
+    setSelectedYear(year);
+    setSelectedMonth(month);
+  };
 
   useEffect(() => {
     const fetchAttendanceStats = async () => {
@@ -71,8 +75,8 @@ export default function StudentAttendance() {
         setIsLoading(true);
         const response = await attendanceApi.getAttendanceRecordsByYearMonth(
           courseId,
-          currentYear,
-          currentMonth,
+          selectedYear,
+          selectedMonth,
         );
 
         if (response && response.data) {
@@ -87,9 +91,9 @@ export default function StudentAttendance() {
 
           setAttendanceData(formattedData);
         }
-      } catch (err) {
-        console.error('월별 출석 기록 데이터를 가져오는 중 오류 발생:', err);
-        console.error('에러 상세 정보:', err.response?.data || err);
+      } catch (error) {
+        const errorMessage = error.response?.data?.message ?? '오류가 발생했습니다.';
+        alert(errorMessage);
       } finally {
         setIsLoading(false);
       }
@@ -98,7 +102,7 @@ export default function StudentAttendance() {
     if (courseId) {
       fetchAttendanceRecordsByYearMonth();
     }
-  }, [courseId]);
+  }, [courseId, selectedYear, selectedMonth]);
 
   // if (!courseId) return <div>코스 정보를 불러올 수 없습니다.</div>;
   // if (isLoading) return <div>로딩 중...</div>;
@@ -148,6 +152,10 @@ export default function StudentAttendance() {
             <div className={styles.legendContainer}>
               <div className={styles.legend}>
                 <div className={styles.legendItem}>
+                  <span className={styles.attendanceIndicator}></span>
+                  <span>출석</span>
+                </div>
+                <div className={styles.legendItem}>
                   <span className={styles.lateIndicator}></span>
                   <span>지각</span>
                 </div>
@@ -162,7 +170,10 @@ export default function StudentAttendance() {
               </div>
             </div>
             <div className={styles.calendarWrapper}>
-              <Calendar attendanceData={attendanceData}></Calendar>
+              <Calendar
+                attendanceData={attendanceCalendarData}
+                onMonthChange={handleMonthChange}
+              ></Calendar>
             </div>
           </DashBoardItem>
         </div>
