52cfc449905d9c04e9e957fd2c5471f65ccaaf3b
yejin <yejinoh58@gmail.com>
2025-04-18 01:04:00 +0900

    [ECR-43] feat: FE UX 개선 - 강의 없는 날짜인 경우, 금일 출결 현황에 안내 문구 (#39)
    랜더링 추가

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -39,9 +39,6 @@ export default function Login() {
     const accessToken = response.headers?.authorization ?? '';
     const userData = response.data?.data;
 
-    console.log(accessToken);
-    console.log(userData);
-
     dispatch(
       login({
         ...userData,
