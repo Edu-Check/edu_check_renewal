d33422b05655580d8e2fe17d455f3a41e0b4e022
ysc13245 <ysc13245@gmail.com>
2025-05-01 10:52:29 +0900

    [ECR-61] fix: 수강생 상태 변경 api 수정 change http method type

--- api/src/main/java/org/example/educheck/domain/member/staff/controller/StaffController.java
+++ api/src/main/java/org/example/educheck/domain/member/staff/controller/StaffController.java
@@ -21,7 +21,7 @@ public class StaffController {
     private final StaffService staffService;
 
     @PreAuthorize("hasAuthority('MIDDLE_ADMIN')")
-    @PutMapping("/course/{courseId}/students/{studentId}")
+    @PatchMapping("/course/{courseId}/students/{studentId}")
     public ResponseEntity<ApiResponse<UpdateStudentRegistrationStatusResponseDto>> updateStudentRegistrationStatus(
             @AuthenticationPrincipal Member member,
             @PathVariable Long courseId,
@@ -46,6 +46,4 @@ public class StaffController {
                         "OK",
                         staffService.getStudentsByCourse(member, courseId)));
     }
-
-
 }
