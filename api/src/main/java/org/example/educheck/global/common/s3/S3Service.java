17d8321dc84972d276406b6993c9812500d26b42
yejin <yejinoh58@gmail.com>
2025-06-18 18:54:17 +0900

    [ECR-75] feat: generatePresignedUrl method 구현

--- api/src/main/java/org/example/educheck/global/common/s3/S3Service.java
+++ api/src/main/java/org/example/educheck/global/common/s3/S3Service.java
@@ -7,17 +7,18 @@ import org.example.educheck.global.common.exception.custom.common.ServerErrorExc
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.exception.SdkException;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.model.S3Exception;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
+import java.time.Duration;
+import java.util.*;
 
 @Slf4j
 @Service
@@ -28,60 +29,45 @@ public class S3Service {
     //버킷명, 리전, 파일경로
     private static final String IMAGE_URL_FORMAT = "https://%s.s3.%s.amazonaws.com/%s";
     private final S3Client s3Client;
+    private final S3Presigner s3Presigner;
     @Value("${BUCKET_NAME}")
     private String bucketName;
     @Value("${REGION}")
     private String region;
 
-    public List<Map<String, String>> uploadFiles(MultipartFile[] files) {
+    public List<Map<String, String>> generatePresignedUrls(String[] fileNames) {
+        List<Map<String, String>> result = new ArrayList<>();
 
-        if (files.length > 5) {
-            return Arrays.stream(files)
-                    .parallel()
-                    .map(this::uploadFile)
-                    .toList();
-        } else {
-            return Arrays.stream(files)
-                    .map(this::uploadFile)
-                    .toList();
+        for (String fileName : fileNames) {
+            try {
+                String s3Key = FILE_PATH_PREFIX + UUID.randomUUID() + "_" + fileName;
+                log.info("s3Key : {}", s3Key);
+
+                PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                        .bucket(bucketName)
+                        .key(s3Key)
+                        .build();
+
+                PutObjectPresignRequest presignRequest = PutObjectPresignRequest.builder()
+                        .signatureDuration(Duration.ofMinutes(10))
+                        .putObjectRequest(putObjectRequest)
+                        .build();
+
+                String presignedUrl = s3Presigner.presignPutObject(presignRequest).url().toString();
+                String fileUrl = String.format(IMAGE_URL_FORMAT, bucketName, region, s3Key);
+
+                result.add(Map.of(
+                        "fileUrl", fileUrl,
+                        "s3Key", s3Key,
+                        "presignedUrl", presignedUrl
+                ));
+            } catch (SdkException e) {
+                log.error("Failed to generate presigned url for file : {}", fileName, e);
+                throw new RuntimeException("Presigned URL 생성 중 에러 발생", e);
+            }
         }
 
-    }
-
-    private Map<String, String> uploadFile(MultipartFile file) {
-        String s3Key = FILE_PATH_PREFIX + UUID.randomUUID() + "_" + file.getOriginalFilename();
-        log.info("s3Key : {}", s3Key);
-
-        uploadFileToS3(file, s3Key);
-
-        String fileUrl = String.format(IMAGE_URL_FORMAT, bucketName, region, s3Key);
-
-        return Map.of(
-                "fileUrl", fileUrl,
-                "s3Key", s3Key
-        );
-    }
-
-    private void uploadFileToS3(MultipartFile file, String s3Key) {
-
-        try {
-            PutObjectRequest putObjectRequest = PutObjectRequest.builder()
-                    .bucket(bucketName)
-                    .key(s3Key)
-                    .contentType(file.getContentType())
-                    .contentLength(file.getSize())
-                    .build();
-
-            s3Client.putObject(putObjectRequest,
-                    RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
-        } catch (IOException | S3Exception e) {
-            log.error(e.getMessage());
-            throw new ServerErrorException(ErrorCode.FILE_UPLOAD_ERROR);
-        } catch (Exception e) {
-            log.error(e.getMessage());
-            throw new ServerErrorException(ErrorCode.FILE_UPLOAD_ERROR);
-        }
-
+        return result;
     }
 
     public void deleteFile(String s3Key) {
@@ -98,6 +84,6 @@ public class S3Service {
         }
     }
 
-
 }
+
 
