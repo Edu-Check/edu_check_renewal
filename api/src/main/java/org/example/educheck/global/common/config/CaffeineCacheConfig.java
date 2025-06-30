f5305882ada694a360adb65db85e67c9960b84f8
yejin <yejinoh58@gmail.com>
2025-06-30 21:36:42 +0900

    [ECR-78] feat: 유고결석 신청 상세 내역 API Cache 적용

--- api/src/main/java/org/example/educheck/global/common/config/CaffeineCacheConfig.java
+++ api/src/main/java/org/example/educheck/global/common/config/CaffeineCacheConfig.java
@@ -4,6 +4,7 @@ import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.concurrent.TimeUnit;
@@ -12,6 +13,7 @@ import java.util.concurrent.TimeUnit;
 @EnableCaching
 public class CaffeineCacheConfig {
 
+    @Bean
     public CacheManager cacheManager() {
         CaffeineCacheManager cacheManager = new CaffeineCacheManager();
         cacheManager.setCaffeine(caffeineCacheBuilder());
