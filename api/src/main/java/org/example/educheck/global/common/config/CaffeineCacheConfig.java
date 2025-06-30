17c773164731a53df1e5f952ff2f08d5ace9595b
yejin <yejinoh58@gmail.com>
2025-06-30 20:49:27 +0900

    [ECR-78] chore: Caffeine Cache config 등록

---
+++ api/src/main/java/org/example/educheck/global/common/config/CaffeineCacheConfig.java
@@ -0,0 +1,27 @@
+package org.example.educheck.global.common.config;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+@EnableCaching
+public class CaffeineCacheConfig {
+
+    public CacheManager cacheManager() {
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager();
+        cacheManager.setCaffeine(caffeineCacheBuilder());
+        return cacheManager;
+    }
+
+    private Caffeine<Object, Object> caffeineCacheBuilder() {
+        return Caffeine.newBuilder()
+                .expireAfterAccess(24, TimeUnit.HOURS)
+                .maximumSize(100)
+                .recordStats();
+    }
+}
