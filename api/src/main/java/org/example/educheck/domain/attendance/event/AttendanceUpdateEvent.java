f903d393114db4a594eea6774c588449700021c9
yejin <yejinoh58@gmail.com>
2025-07-09 15:39:08 +0900

    [ECR-82] refactor: AttendanceUpdateEvent -> AttendanceUpdatedEvent
    - 클래스명 수정
    - 기존 상태, 새로운 상태 필드 추가

--- api/src/main/java/org/example/educheck/domain/attendance/event/AttendanceUpdateEvent.java
+++ api/src/main/java/org/example/educheck/domain/attendance/event/AttendanceUpdateEvent.java
@@ -4,11 +4,16 @@ package org.example.educheck.domain.attendance.event;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.example.educheck.domain.attendance.entity.Attendance;
+import org.example.educheck.domain.attendance.entity.AttendanceStatus;
 
+@Getter
 @RequiredArgsConstructor
 public class AttendanceUpdateEvent {
 
-    @Getter
     private final Attendance attendance;
+
+    private final AttendanceStatus oldStatus;
+
+    private final AttendanceStatus newStatus;
 
 }
