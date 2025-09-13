3089633764b26e66f61161dde4dd8fb7398320cd
yejin <yejinoh58@gmail.com>
2025-09-14 08:54:29 +0900

    [ECR-86] feat: RabbitMQ default Converter -> Jackson2JsonMessageConverter 적용
    - POJO <-> JSON 변환을 위해 설정

--- api/src/main/java/org/example/educheck/global/common/config/RabbitMQConfig.java
+++ api/src/main/java/org/example/educheck/global/common/config/RabbitMQConfig.java
@@ -1,9 +1,14 @@
 package org.example.educheck.global.common.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -29,6 +34,18 @@ public class RabbitMQConfig {
         return BindingBuilder.bind(courseNoticeQueue)
                 .to(courseExchange)
                 .with(ROUTING_KEY);
+    }
+
+    @Bean
+    public MessageConverter Jackson2JsonMessageConverter() {
+        return new Jackson2JsonMessageConverter(new ObjectMapper());
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, MessageConverter messageConverter) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter);
+        return rabbitTemplate;
     }
 
 }
