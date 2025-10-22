efdfa54d03e2134c9758925a2e4697b6ecc293ba
yejin <yejinoh58@gmail.com>
2025-10-22 13:33:18 +0900

    [ECR-94] feat: retry 적용

--- api/src/main/java/org/example/educheck/global/rabbitmq/RabbitMQNoticeAdapter.java
+++ api/src/main/java/org/example/educheck/global/rabbitmq/RabbitMQNoticeAdapter.java
@@ -4,8 +4,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.educheck.domain.notice.port.out.NoticeEventPublisherPort;
 import org.example.educheck.global.rabbitmq.dto.NoticeMessageDto;
+import org.springframework.amqp.AmqpException;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -21,12 +25,24 @@ public class RabbitMQNoticeAdapter implements NoticeEventPublisherPort {
 
     private final RabbitTemplate rabbitTemplate;
 
-
+    @Retryable(
+            value = {AmqpException.class},
+            maxAttempts = 2,
+            backoff = @Backoff(delay = 1000, multiplier = 2)
+    )
     @Override
     public void publish(NoticeMessageDto messageDto) {
         String routingKey = getRoutingKey(messageDto.getCourseId());
-        log.info("rabbitmq event publish");
-        rabbitTemplate.convertAndSend(exchangeName, routingKey, messageDto);
+            rabbitTemplate.convertAndSend(exchangeName, routingKey, messageDto);
+    }
+
+    @Recover
+    public void recover(AmqpException e, NoticeMessageDto messageDto) {
+        log.error("재시도 후 최종 실패 - courseId: {}", messageDto.getCourseId(), e);
+
+        //fail 테이블에 저장
+
+        //TODO: 슬랙알림 연동
     }
 
     private String getRoutingKey(Long courseId) {
